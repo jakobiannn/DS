25f9694af3efc1e6544ae51d3d156161b610c472
jakobiannn <mozilla322@gmail.com>
2019-09-29 20:51:32 +0300

    Лабораторная №3 с ошибкой

--- Lab3.pas
+++ Lab3.pas
@@ -1,5 +1,5 @@
 ﻿Program lab3;
-
+uses crt;
 type
   TDate = class
     private
@@ -8,12 +8,11 @@ type
     procedure inputAll(Hour,Min,Sec:Integer); //Ввод времени
     public
     
-    function DateDiff(h1,m1,s1, h2,m2,s2: Integer):String[100]; //Разница между двумя показателями времени в одни сутки
-    function Print(h1,m1,s1:integer):String; //Вывод текущего заданного времени
-    function DateAdd(h1,m1,s1, h2,m2,s2: Integer):String; //Сумма двух временных показателей
-    function DateDiffDays(day1,h1,m1,s1, day2,h2,m2,s2: Integer):String; //Разница между двумя показателями времени в разные дни
+    procedure DateDiff(h1,m1,s1, h2,m2,s2: Integer; var Answer:String[250]); //Разница между двумя показателями времени в одни сутки
+    procedure Print(h1,m1,s1:Integer; var Answer:String[250]); //Вывод текущего заданного времени
+    procedure DateAdd(h1,m1,s1, h2,m2,s2: Integer; var Answer:String[250]); //Сумма двух временных показателей
+    procedure DateDiffDays(day1,h1,m1,s1, day2,h2,m2,s2: Integer; var Answer:String[250]); //Разница между двумя показателями времени в разные дни
     end;
-
 
 procedure TDate.inputAll(Hour,Min,Sec: Integer);
   begin
@@ -25,7 +24,7 @@ procedure TDate.inputAll(Hour,Min,Sec: Integer);
     readln(sec);
   end;
 
-function TDate.Print(h1,m1,s1: Integer):String;
+procedure TDate.Print(h1,m1,s1: Integer; var Answer:String[250]);
   var
     insec,inmin,inhours: Real;
   begin
@@ -33,26 +32,26 @@ function TDate.Print(h1,m1,s1: Integer):String;
    insec := h1*60*60+m1*60+s1;
    inmin := h1*60+m1+s1/60;
    inhours := h1+m1/60+(s1/60)/60;
-   Print := inttostr(h1) + ' hour, ' + inttostr(m1) + ' min, ' + inttostr(s1) + ' sec. (' + floattostr(insec) +
+   Answer := inttostr(h1) + ' hour, ' + inttostr(m1) + ' min, ' + inttostr(s1) + ' sec. (' + floattostr(insec) +
     ') sec, (' + floattostr(inmin) + ') min, (' + floattostr(inhours) + ') hours.';
   end;
 
-function TDate.DateDiff(h1,m1,s1, h2,m2,s2: Integer):String[100];
+procedure TDate.DateDiff(h1,m1,s1, h2,m2,s2: Integer; var Answer:String[250]);
   begin
     inputALL(h1,m1,s1);
     inputALL(h2,m2,s2);
     
-    DateDiff  := inttostr(abs(h1-h2)) + ' hour, ' + inttostr(abs(m1-m2)) + ' min, ' + inttostr(abs(s1-s2))+ ' sec.';
+    Answer  := inttostr(abs(h1-h2)) + ' hour, ' + inttostr(abs(m1-m2)) + ' min, ' + inttostr(abs(s1-s2))+ ' sec.';
   end;
 
-function TDate.DateAdd(h1,m1,s1, h2,m2,s2: Integer):String;
+procedure TDate.DateAdd(h1,m1,s1, h2,m2,s2: Integer; var Answer:String[250]);
   begin
     inputALL(h1,m1,s1);
     inputALL(h2,m2,s2);
-    DateAdd := inttostr(abs(h1+h2))+ ' hour, ' + inttostr(abs(m1+m2))+ ' min, ' + inttostr(abs(s1+s2))+ ' sec.';
+    Answer := inttostr(abs(h1+h2))+ ' hour, ' + inttostr(abs(m1+m2))+ ' min, ' + inttostr(abs(s1+s2))+ ' sec.';
   end;
 
-function TDate.DateDiffDays(day1,h1,m1,s1, day2,h2,m2,s2: Integer): String;
+procedure TDate.DateDiffDays(day1,h1,m1,s1, day2,h2,m2,s2: Integer; var Answer:String[250]);
   var
     y,z: integer;
   begin
@@ -61,12 +60,12 @@ function TDate.DateDiffDays(day1,h1,m1,s1, day2,h2,m2,s2: Integer): String;
     z := y + abs(h1-h2);
     if z < 24 then
       begin
-      DateDiffDays :=  
+      Answer :=  
       inttostr(z)+ ' hour, ' + inttostr(abs(m1-m2))+ ' min, ' + inttostr(abs(s1-s2))+ ' sec.';
       end
     else 
       begin
-        DateDiffDays := 
+        Answer := 
         inttostr(z div 24) + ' days, ' + inttostr(z mod 24)+ ' hour, ' + inttostr(abs(m1-m2))+ ' min, ' + inttostr(abs(s1-s2))+ ' sec.';
       end;
 
@@ -74,7 +73,7 @@ function TDate.DateDiffDays(day1,h1,m1,s1, day2,h2,m2,s2: Integer): String;
 
 var
   Date1: TDate;
-  Answer: String;
+  Answer: String[250];
   h1,m1,s1,h2,m2,s2,d1,d2, n: Integer;
 
 begin
@@ -87,69 +86,74 @@ begin
   readln(n);
   case n of
     1:  begin 
-          writeln('Write 1st date hours');
+          write('Write 1st date hours: ');
           readln(h1);
-          writeln('Write 1st date minutes');
+          write('Write 1st date minutes: ');
           readln(m1);
-          writeln('Write 1st date seconds');
+          write('Write 1st date seconds: ');
           readln(s1);
-          writeln('Write 2nd date hours');
+          write('Write 2nd date hours: ');
           readln(h2);
-          writeln('Write 2nd date minutes');
+          write('Write 2nd date minutes: ');
           readln(m2);
-          writeln('Write 2nd date seconds');
+          write('Write 2nd date seconds: ');
           readln(s2);
-          writeln('Difference is ', Date1.DateDiff(h1,m1,s1, h2,m2,s2));
+            clrscr;
+          Date1.DateDiff(h1,m1,s1, h2,m2,s2, Answer);
+          writeln('Difference is ', Answer); 
+          // ↑ Ошибка времени выполнения: Ссылка на объект не указывает на экземпляр объекта.
         end;
     2:  begin
-          writeln('Write 1st date day');
+          write('Write 1st date day: ');
           readln(d1);
-          writeln('Write 1st date hours');
+          write('Write 1st date hours: ');
           readln(h1);
-          writeln('Write 1st date minutes');
+          write('Write 1st date minutes: ');
           readln(m1);
-          writeln('Write 1st date seconds');
+          write('Write 1st date seconds: ');
           readln(s1);
-          writeln('Write 2nd date day');
+          write('Write 2nd date day: ');
           readln(d2);
-          writeln('Write 2nd date hours');
+          write('Write 2nd date hours: ');
           readln(h2);
-          writeln('Write 2nd date minutes');
+          write('Write 2nd date minutes: ');
           readln(m2);
-          writeln('Write 2nd date seconds');
+          write('Write 2nd date seconds: ');
           readln(s2);
-          writeln('Difference is ', Date1.DateDiffDays(d1,h1,m1,s1, d2,h2,m2,s2));
+            clrscr;
+          Date1.DateDiffDays(d1,h1,m1,s1, d2,h2,m2,s2, Answer);
+          writeln('Difference is ', Answer);
+          // ↑ Ошибка времени выполнения: Ссылка на объект не указывает на экземпляр объекта.
         end;
     3:  begin
-          writeln('Write 1st date hours');
+          write('Write 1st date hours: ');
           readln(h1);
-          writeln('Write 1st date minutes');
+          write('Write 1st date minutes: ');
           readln(m1);
-          writeln('Write 1st date seconds');
+          write('Write 1st date seconds: ');
           readln(s1);
-          writeln('Write 2nd date hours');
+          write('Write 2nd date hours: ');
           readln(h2);
-          writeln('Write 2nd date minutes');
+          write('Write 2nd date minutes: ');
           readln(m2);
-          writeln('Write 2nd date seconds');
+          write('Write 2nd date seconds: ');
           readln(s2);
-          writeln('Summ of dates is ', Date1.DateAdd(h1,m1,s1, h2,m2,s2));
+            clrscr;
+          Date1.DateAdd(h1,m1,s1, h2,m2,s2, Answer);
+          writeln('Summ of dates is ', Answer);
+          // ↑ Ошибка времени выполнения: Ссылка на объект не указывает на экземпляр объекта.
         end;
     4:  begin
-          writeln('Write 1st date hours');
+          write('Write 1st date hours: ');
           readln(h1);
-          writeln('Write 1st date minutes');
+          write('Write 1st date minutes: ');
           readln(m1);
-          writeln('Write 1st date seconds');
+          write('Write 1st date seconds: ');
           readln(s1);
-          writeln('Write 2nd date hours');
-          readln(h2);
-          writeln('Write 2nd date minutes');
-          readln(m2);
-          writeln('Write 2nd date seconds');
-          readln(s2);
-
-          writeln('Difference is ', Date1.Print(h1,m1,s1));
+            clrscr;
+          Date1.Print(h1,m1,s1, Answer);
+          writeln('Difference is ', Answer);
+          // ↑ Ошибка времени выполнения: Ссылка на объект не указывает на экземпляр объекта.
         end;
   end;
 
